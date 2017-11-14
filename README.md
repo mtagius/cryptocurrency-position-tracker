77e7216d5ff61671220e4ad37505853ba45e2b80
Matthew Agius <mtagius@gmail.com>
2017-11-14 18:26:13 -0500

    barebones structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cryptocurrency-position-tracker
 This web app lets you enter a position of cryptocurrency and shows the change in value.
+
+Coin prices are retrieved from the cryptocompare api
+https://www.cryptocompare.com/api
--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+my = "";
+
 $(document).ready(function() {
+    /*
     $.ajax({
         url: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,ETH&tsyms=USD&e=GDAX",
         type: "GET",
@@ -12,5 +15,10 @@ $(document).ready(function() {
         success: function(result){
 
         }
+    });
+    */
+    $.getJSON("../data/positions.json?nocache="+new Date(), function(positions) {
+        my = positions;
+        alert(my.positions[0].ticker);
     });
 });
