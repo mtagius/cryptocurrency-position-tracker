a1221798ca259d9e43f2010814dc84cd675b1e65
Matthew Agius <mtagius@gmail.com>
2017-11-17 20:08:33 -0500

    added sorting and other table metrics

--- js/main.js
+++ js/main.js
@@ -95,11 +95,23 @@ function nameToTicker(name) {
     return null;
 }
 
+function sortByKey(key) {
+    return my.positions.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
 function generateTable() {
     canRefresh = true;
     if(ajaxCalls == 0) {
-        var table = "<table><tr><th>Icon</th><th>Coin</th><th>Ticker</th><th>Current Price</th><th>Coin Amount</th>" +
-            "<th>Purchase Price</th><th>Fees</th><th>Percent Change</th><th>Gross</th><th>Net</th></tr>";
+        var table = "<table><tr><th></th><th>Coin</th><th>Ticker</th><th>Current Price</th><th>Coin Amount</th>" +
+            "<th>Purchase Price</th><th>Fees</th><th>Initial Investment</th><th>Current Worth</th><th>Percent Change</th><th>Gross</th><th>Net</th><th></th></tr>";
+            var totalGross = 0;
+            var totalNet = 0;
+            var totalPurchasePrice = 0;
+            var totalCurrentPrice = 0;
+            var textClass = "";
         for(var i = 0; i < my.positions.length; i++) {
             var coinPrice = 0;
             if(gdaxResults != null) {
@@ -111,19 +123,55 @@ function generateTable() {
             } else {
                 coinPrice = otherResults[my.positions[i].ticker].USD;
             }
+            var percentChange = (((coinPrice - my.positions[i].purchasePrice) / my.positions[i].purchasePrice) * 100);
+            if(percentChange >= 0) {
+                textClass = "make-it-rain";
+            } else {
+                textClass = "losing-money";
+            }
+            totalPurchasePrice += (my.positions[i].purchasePrice * my.positions[i].coinAmount);
+            totalCurrentPrice += (coinPrice  * my.positions[i].coinAmount);
+            var gross = ((coinPrice * my.positions[i].coinAmount) - (my.positions[i].purchasePrice * my.positions[i].coinAmount));
+            totalGross += gross;
+            var net = (((coinPrice * my.positions[i].coinAmount) - (my.positions[i].purchasePrice * my.positions[i].coinAmount)) - (my.positions[i].fee));
+            totalNet += net;
             table += "<tr>";
             table += "<td><img src='img/" + tickerToName(my.positions[i].ticker).toLowerCase() + ".png'></td>";
             table += "<td>" + tickerToName(my.positions[i].ticker) + "</td>";
             table += "<td>" + my.positions[i].ticker + "</td>";
-            table += "<td>$" + coinPrice + "</td>";
+            table += "<td>$" + coinPrice.toFixed(2) + "</td>";
             table += "<td>" + my.positions[i].coinAmount + "</td>";
-            table += "<td>$" + my.positions[i].purchasePrice + "</td>";
-            table += "<td>$" + my.positions[i].fee + "</td>";
-            table += "<td>" + (((coinPrice - my.positions[i].purchasePrice) / my.positions[i].purchasePrice) * 100).toFixed(2) + "%</td>";
-            table += "<td>$" + ((coinPrice * my.positions[i].coinAmount) - (my.positions[i].purchasePrice * my.positions[i].coinAmount)).toFixed(2) + "</td>";
-            table += "<td>$" + (((coinPrice * my.positions[i].coinAmount) - (my.positions[i].purchasePrice * my.positions[i].coinAmount)) - (my.positions[i].fee)).toFixed(2) + "</td>";
+            table += "<td>$" + parseFloat(my.positions[i].purchasePrice).toFixed(2) + "</td>";
+            table += "<td>$" + parseFloat(my.positions[i].fee).toFixed(2) + "</td>";
+            table += "<td>$" + (my.positions[i].purchasePrice * my.positions[i].coinAmount).toFixed(2)  + "</td>";
+            table += "<td>$" + (coinPrice * my.positions[i].coinAmount).toFixed(2)  + "</td>";
+            table += "<td class='" + textClass + "'>" + percentChange.toFixed(2) + "%</td>";
+            table += "<td class='" + textClass + "'>$" + gross.toFixed(2) + "</td>";
+            table += "<td class='" + textClass + "'>$" + net.toFixed(2) + "</td>";
+            table += "<td><span class='glyphicon glyphicon-remove' onclick='removePosition(" + i + ")'></span></td>";
             table += "</tr>";
         }
+        var totalPercent = (((totalCurrentPrice - totalPurchasePrice) / totalPurchasePrice) * 100);
+        table += "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td>Totals:</td><td>$" + 
+            totalPurchasePrice.toFixed(2) + "</td><td>$" + totalCurrentPrice.toFixed(2) + "</td>";
+        if(totalPercent >= 0) {
+            textClass = "make-it-rain";
+        } else {
+            textClass = "losing-money";
+        }
+        table += "<td class='" + textClass + "'>" + totalPercent.toFixed(2) + "%</td>";
+        if(totalGross >= 0) {
+            textClass = "make-it-rain";
+        } else {
+            textClass = "losing-money";
+        }
+        table += "<td class='" + textClass + "'>$" + totalGross.toFixed(2) + "</td>";
+        if(totalNet >= 0) {
+            textClass = "make-it-rain";
+        } else {
+            textClass = "losing-money";
+        }
+        table += "<td class='" + textClass + "'>$" + totalNet.toFixed(2) + "</td></tr>";
         table += "</table>"
         $("#mainTable").html(table);
         console.log("Wrote Table to Page");
@@ -136,6 +184,7 @@ function readPositions() {
     canRefresh = false;
     $.getJSON("data/positions.json?nocache="+new Date(), function(positions) {
         my = positions;
+        my.positions = sortByKey("ticker");
         tickers = [];
         for(var i = 0; i < my.positions.length; i++) {
             tickers.push(my.positions[i].ticker);
