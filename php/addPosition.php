12e312b8f6c0c38b8d58d3d749d76fde763584eb
Matthew Agius <mtagius@gmail.com>
2017-12-12 12:46:06 -0500

    updated posistions sort to also use coinAmount

--- php/addPosition.php
+++ php/addPosition.php
@@ -3,11 +3,15 @@ $json_array = json_decode(file_get_contents('../data/positions.json'), true);
 
 array_push($json_array['positions'], $_POST);
 
-function sortByTicker($a, $b) {
-    return ($a['ticker']<$b['ticker'])?-1:1;
+function sortByTickerandCoinAmount($a, $b) {
+    if($a['ticker'] == $b['ticker']) {
+        return ($a['coinAmount'] > $b['coinAmount']) ? -1 : 1;
+    } else {
+        return ($a['ticker'] < $b['ticker']) ? -1 : 1;
+    }
 }
 
-usort($json_array['positions'], 'sortByTicker');
+usort($json_array['positions'], 'sortByTickerandCoinAmount');
 
 file_put_contents('../data/positions.json', json_encode($json_array));
 ?>
